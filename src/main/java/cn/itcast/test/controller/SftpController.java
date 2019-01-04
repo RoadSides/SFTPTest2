800a4064583c8663fffbfd2294bcf350b905fa59
RoadSides <1813568679@qq.com>
2019-01-04 21:21:22 +0800

    测试工具提交代码

--- src/main/java/cn/itcast/test/controller/SftpController.java
+++ src/main/java/cn/itcast/test/controller/SftpController.java
@@ -4,6 +4,7 @@ import cn.itcast.test.model.RestResult;
 import cn.itcast.test.model.SFTPConfig;
 import cn.itcast.test.service.SftpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class SftpController {
     @Autowired
     private SftpService sftpService;
 
-    @RequestMapping("/testFtpConnect")
+    @GetMapping("/testFtpConnect")
     public RestResult testFtpConnect(@RequestParam String path){
         SFTPConfig sftpConfig = sftpService.testFtpConnect(path);
         return new RestResult(200,"success",sftpConfig);
